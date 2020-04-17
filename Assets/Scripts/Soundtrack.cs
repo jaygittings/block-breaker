633a5cff9b980d6b77fbdc3b3d0d200098335fff
jaygittings <jaygittings@yahoo.com>
2020-04-17 16:26:34 -0400

    Improved sound, UI
    Added soundtrack prefab
    Improve ball sound effects
    Refactored UI elements, added sfx events

---
+++ Assets/Scripts/Soundtrack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soundtrack : MonoBehaviour
+{
+
+    [SerializeField] AudioClip[] songList = null;
+
+    AudioSource player = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<AudioSource>();
+        player.clip = songList[0];
+        player.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UIAudio.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIAudio : MonoBehaviour
+{
+    [SerializeField] GameObject mouseOver = null;
+    [SerializeField] GameObject mouseClick = null;
+
+    AudioSource mouseOverAudio = null;
+    AudioSource mouseClickAudio = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (mouseOver != null)
+            mouseOverAudio = mouseOver.GetComponent<AudioSource>();
+        if (mouseClick != null)
+            mouseClickAudio = mouseClick.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayOnMouseClick()
+    {
+        mouseClickAudio.PlayOneShot(mouseClickAudio.clip);
+    }
+
+    public void PlayOnMouseOver()
+    {
+        mouseOverAudio.PlayOneShot(mouseOverAudio.clip);
+    }
+}
